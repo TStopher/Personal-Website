0b73d637749f497aae44bd4ecdb77dd75694ee68
TStopher <115030113+TStopher@users.noreply.github.com>
2023-04-18 09:15:38 -0600

    bug fixes
    the original js had problems such as incrementally changing the interests instead of doing it once, changing positions of interests way too fast, and overall just being bloated. I think this works a lot better.

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-            /*For moving the elements on the about page*/
-            document.addEventListener('DOMContentLoaded', () => {
-            const container = document.querySelector('.interest-container');
-            const interests = document.querySelectorAll('.interest');
-            
-            container.addEventListener('mousemove', (event) => {
-              const containerRect = container.getBoundingClientRect();
-              const centerX = containerRect.left + containerRect.width / 2;
-              const centerY = containerRect.right + containerRect.height / 2;
-              const mouseX = event.clientX;
-              const mouseY = event.clientY;
-              const distanceX = mouseX - centerX;
-              const distanceY = mouseY - centerY;
-            
-              interests.forEach((interest) => {
-                const factor = 0.1; 
-                const translateX = distanceX * factor * interest.dataset.translation;
-                interest.style.transform = `translate(${translateX}px`;
-              });
-            });
-            
-            
-            interests.forEach((interest, index) => {
-              interest.dataset.translation = (index % 2 === 0) ? -1 : 1;
-            });
-          });
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.interest-container');
+  const interests = document.querySelectorAll('.interest');
+  let lastX = 0;
+
+  container.addEventListener('mousemove', (event) => {
+    const containerRect = container.getBoundingClientRect();
+    const centerX = containerRect.left + containerRect.width / 2;
+    const mouseX = event.clientX;
+
+    // Check if the mouse has moved to the left or right side of the container
+    if (mouseX < centerX && lastX >= centerX) {
+      moveInterests(-1); 
+    } else if (mouseX > centerX && lastX <= centerX) {
+      moveInterests(1); 
+    }
+
+    lastX = mouseX;
+  });
+
+  // Move the interests to the left or right based on the given direction
+  function moveInterests(direction) {
+    interests.forEach((interest, index) => {
+      const factor = (index % 2 === 0) ? 0.3 : 0.35; // adjust this to control the amount of translation
+      const translateX = direction * container.offsetWidth * factor;
+      interest.style.transform = `translateX(${translateX}px)`;
+    });
+  }
+});
